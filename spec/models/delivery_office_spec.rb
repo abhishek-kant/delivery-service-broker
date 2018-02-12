57522f798b640f7327cb8c5605c14f1edc06f27a
Abhishek kant <abhi1206118@gmail.com>
2018-02-13 03:03:46 +0800

    model specs

--- spec/models/delivery_office_spec.rb
+++ spec/models/delivery_office_spec.rb
@@ -1,6 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe DeliveryOffice, type: :model do
+  let(:new_delivery_office) { build(:delivery_office, :office_1) }
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:postcode) }
+    it { expect(create(:delivery_office, :office_1)).to validate_uniqueness_of(:postcode) }
+  end
+
+  describe "callbacks" do
+    it "executes fetch_and_save_lon_lat after save" do
+      expect(new_delivery_office).to receive(:fetch_and_save_lon_lat)
+      new_delivery_office.run_callbacks :save
+    end
+  end
+
+  describe "fetch_and_save_lon_lat" do
+    it "saves lonlat after save" do
+      expect(new_delivery_office.lonlat).to be(nil)
+      new_delivery_office.run_callbacks :save
+      pio = Postcodes::IO.new
+      postcode_response = pio.lookup(new_delivery_office.postcode)
+      expected_lonlat = "POINT(#{postcode_response.longitude} #{postcode_response.latitude})"
+      expect(new_delivery_office.lonlat.as_text.gsub(" ","")).to eq(expected_lonlat.strip.gsub(" ",""))
+    end
+  end
+
+
+
+
   describe "search_in_radius" do
     before(:each) do
       @office_1 = create(:delivery_office, :office_1)
